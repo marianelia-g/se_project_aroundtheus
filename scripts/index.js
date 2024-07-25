8b578fd8dbc71892823c9552bc9042ef4e404674
Marianelia <marianelia1405@gmail.com>
2024-07-24 21:06:22 -0400

    Add addEventLister to profile edit button

--- scripts/index.js
+++ scripts/index.js
@@ -25,4 +25,16 @@ const initialCards = [
   },
 ];
 
-console.log(initialCards);
+// Elements
+
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileCloseModal = document.querySelector("#profile-close-modal");
+
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal_opened");
+});
+
+profileCloseModal.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal_opened");
+});
